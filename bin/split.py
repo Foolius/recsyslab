0a277b23d61c52b40c580db60499c7c45f70b792
Julius Kolbe <Foolius@server.fake>
2013-03-19 18:05:01 +0100

    forgot split.py

---
+++ bin/split.py
@@ -0,0 +1,16 @@
+import random
+import copy
+
+def split(R,seed):
+
+	random.seed(seed)
+	split={}
+	training={}
+
+	for user in R.iterkeys():
+		item=random.sample(R[user],1)[0] #because random.sample gives a list
+		split[user]=set([item])
+		s=copy.deepcopy(R[user])
+		s.discard(item)
+		training[user]=s
+	return R,split
