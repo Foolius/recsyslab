314fa8a11dfd530377e642c24d1d4dac9ab65886
Julius Kolbe <Foolius@server.fake>
2013-04-24 15:32:55 +0200

    iFixed the rating.

--- bin/mf.py
+++ bin/mf.py
@@ -88,13 +88,15 @@ def learnModel(n_users, m_items, regU, regI, regJ,
             if len(R[u]) == 0:
                 continue
             # the positive example
-            userItems = list(R[u])
-            i = userItems[np.random.random_integers(0, len(userItems) - 1)][0]
+            userItems = [x[0] for x in R[u]]
+#            i = userItems[np.random.random_integers(0, len(userItems) - 1)][0]
+            i = userItems[np.random.random_integers(0, len(userItems) - 1)]
             # the negative example
             j = np.random.random_integers(0, m_items)
             # if  j is also relevant for u we continue
             # we need to see a negative example to contrast the positive one
-            while [x for x in R[u] if x[0] == j]:
+#            while [x for x in R[u] if x[0] == j]:
+            while j in userItems:
                 j = np.random.random_integers(0, m_items)
 
             X = H[i] - H[j]
