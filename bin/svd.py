37c6d954f3e2d541dbd75c8440a65979b5e8eb64
Julius Kolbe <Foolius@server.fake>
2013-04-30 17:34:19 +0200

    Leftovers: testing just to try stuff out. svd: runnable but produce too great values, even getting NaN.

--- bin/svd.py
+++ bin/svd.py
@@ -9,30 +9,68 @@ def learnModel(n_users, m_items, learningRate, R, features,
     userFeatures = sigma * np.random.randn(m_items, features)
     itemFeatures = sigma * np.random.randn(m_items, features)
 
+    printDelay = 0.1 * numberOfIterations
+    sum_loss = 0.0
+
     for e in xrange(0, epochs):
+        iter = 0
+        t = 0
         while iter <= numberOfIterations:
+            iter += 1
             # Choose an user randomly
             u = random.choice(R.keys())
             if len(R[u]) == 0:
                 continue
             # Choose an item the user interacted with
-            i = random.choice(R[u])
+            userItems = [x[0] for x in R[u]]
+            i = random.choice(userItems)
 
-            i0 = np.random.random_integers(0, m_items)
-            while i0 in R.getR()[u]:
-                i0 = np.random.random_integers(0, m_items)
+            i0 = np.random.random_integers(0, m_items - 1)
+            while i0 in userItems:
+                i0 = np.random.random_integers(0, m_items - 1)
 
             ruipred = userFeatures[u].dot(itemFeatures[i])
             rui0pred = userFeatures[u].dot(itemFeatures[i0])
             rui0 = 0
-            e = (ruipred - rui0pred) - (userItemMatrix[u, i] - rui0)
+            for r in R[u]:
+                if r[0] == i:
+                    rui = r[1]
 
+            loss = (ruipred - rui0pred) - (rui - rui0)
+            sum_loss += loss
+#            print "userFeatures[u]", userFeatures[u]
+#            print "itemFeatures[i]", itemFeatures[i]
+#            print "itemFeatures[i0]", itemFeatures[i0]
+#            print "ruipred", ruipred
+#            print "rui0pred", rui0pred
+#            print "rui", rui
+#            print "rui0", rui0
+#            print "loss", loss
+#            print "-----------------"
+
+            from math import isnan
+            if isnan(ruipred) or isnan(rui0pred) or isnan(rui) or isnan(
+                    rui0) or isnan(loss):
+                raise NameError
 
             for k in xrange(0, features):
                 c = userFeatures[u, k]
-                userFeatures[u, k] -= learningRate * (e * (
+                userFeatures[u, k] -= learningRate * (loss * (
                     itemFeatures[i, k] - itemFeatures[i0, k]))
-                itemFeatures[i, k] -= learningRate * (e * c)
-                itemFeatures[i0, k] -= learnModel * (e * (-c))
+                itemFeatures[i, k] -= learningRate * (loss * c)
+                itemFeatures[i0, k] -= learningRate * (loss * (-c))
+
+            scalingFactor = 1E-3
+            userFeatures[u] *= scalingFactor
+            itemFeatures[i] *= scalingFactor
+            itemFeatures[i0] *= scalingFactor
+
+            t += 1  # increment the iteration
+            if t % printDelay == 0:
+                print("Epoch: %i/%i | iteration %i/%i | learning rate=%f"
+                      " | average_loss for the last %i iterations = %f" %
+                     (e + 1, epochs, t, numberOfIterations, learningRate,
+                      printDelay, sum_loss / printDelay))
+                sum_loss = 0.0
 
     return userFeatures, itemFeatures
--- bin/testing.py
+++ bin/testing.py
@@ -1,3 +1,28 @@
+def svd():
+    import reader
+    r = reader.tabSepReader("u.data")
+
+    import split
+    train, test1 = split.split(r.getR(), 1234567890)
+
+    import svd
+    W, H = svd.learnModel(r.getMaxUid(), r.getMaxIid(),
+                          0.00001, train, 75, 1, r.numberOfTransactions)
+    import numpy as np
+    np.savez_compressed(
+        "SVDModelFile", W=W, H=H)
+    # modelf = np.load("BPRMFModelFile" + ".npz")
+    # W = modelf['W']
+    # H = modelf['H']
+    # modelf.close
+
+    import test
+    t = test.MFtest(W, H, train)
+    test.hitrate(test1, t.getRec, 10)
+
+# svd()
+
+
 def mf():
     import reader
     r = reader.tabSepReader("u.data")
@@ -6,27 +31,22 @@ def mf():
     train, test1 = split.split(r.getR(), 1234567890)
 
     import mf
-    # W, H = mf.learnModel(r.getMaxUid(), r.getMaxIid(), 0.01, 0.01, 0.01,
-    #                     0.1, train, 150, 3, r.numberOfTransactions,
-    #                     mf.logLoss, mf.dLogLoss)
+    W, H = mf.learnModel(r.getMaxUid(), r.getMaxIid(), 0.01, 0.01, 0.01,
+                         0.1, train, 150, 3, r.numberOfTransactions,
+                         mf.logLoss, mf.dLogLoss)
     import numpy as np
     # np.savez_compressed(
     #    "BPRMFModelFile", W=W, H=H)
-    modelf = np.load("BPRMFModelFile" + ".npz")
-    W = modelf['W']
-    H = modelf['H']
-    modelf.close
+    # modelf = np.load("BPRMFModelFile" + ".npz")
+    # W = modelf['W']
+    # H = modelf['H']
+    # modelf.close
 
     import test
     t = test.MFtest(W, H, train)
-    # test.hitrate(test1, t.getRec, 10)
-    scoredict = t.getRec(201, 10)
-    for i in scoredict.keys():
-        print i, scoredict[i], test.getScoreMF(r.uidDict.getOrig(201),
-                                               r.iidDict.getOrig(i), W, H, r)
-    print len(scoredict)
+    test.hitrate(test1, t.getRec, 10)
 
-# mf()
+mf()
 
 
 def knn():
@@ -42,7 +62,7 @@ def knn():
     import test
     print test.f1(test1, k.getRec, 10)
 
-#knn()
+# knn()
 
 
 def simple():
@@ -57,5 +77,3 @@ def simple():
 
     import test
     test.auc(test1, c.getRec, r)
-
-simple()
