68d7f1e98b03dc8f20f009275ca43ecd153dcad4
Julius Kolbe <Foolius@server.fake>
2013-05-10 15:37:18 +0200

    Added a docstring to the bin package.

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,1 +1,7 @@
+"""Package with the Python scripts of the recsyslab toolbox.
+
+It contains the Packages:
+    recommender -- Package for the recommender algorithms
+    util        -- Package with all the other stuff needed to try out the
+                    recommender algorithms"""
 pass
