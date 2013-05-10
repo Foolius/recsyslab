4bb3bb88ad2d046c8693c7f7b1fe364b53811a7a
Julius Kolbe <Foolius@server.fake>
2013-05-10 17:16:06 +0200

    Added many docstrings.

--- bin/util/reader.py
+++ bin/util/reader.py
@@ -3,8 +3,17 @@ import numpy as np
 
 
 class stringSepReader(object):
+    """A class to manage a database."""
 
     def __init__(self, filename, separator):
+        """Reads in a database file.
+
+        The lines of the database file has to look like this:
+            UserID<separator>ItemID<separator>Rating
+        If there is just an UserID and an ItemID the rating is set to 1.
+        Everything coming after the rating will be ignored,
+        but when omit the rating and still have something following the ItemID
+        this will be understood as the rating."""
         self.R = {}
         self.uidDict = helper.idOrigDict()
         self.iidDict = helper.idOrigDict()
@@ -43,20 +52,44 @@ class stringSepReader(object):
                 self.matrix[u, item] = rating
 
     def getR(self):
+        """Return the database as a dict.
+
+        The dict has internal UserIDs as keys and
+        (ItemID, Rating) Tuples as values"""
         return self.R
 
     def getMatrix(self):
-        """user x items"""
+        """Get the database as a matrix.
+
+        The lines of the matrix are corresponding to the users
+        and the columns to the items.
+        So the n,m entry is the rating the nth user gave the mth item."""
         return self.matrix
 
-    def getOriginalUid(self, uid):
-        return self.uidDict.getOrig(uid)
+    def getOriginalUid(self, internalUid):
+        """Maps the given internal UserID to the corresponding original UserID.
+        """
+        return self.uidDict.getOrig(internalUid)
 
-    def getOriginalIid(self, iid):
-        return self.iidDict.getOrig(iid)
+    def getOriginalIid(self, internalIid):
+        """Maps the given internal ItemID to the corresponding original ItemID.
+        """
+        return self.iidDict.getOrig(internalIid)
+
+    def getInternalUid(self, originalUid):
+        """Maps the given original UserID to the corresponding internal UserID.
+        """
+        return self.uidDict.getId(originalUid)
+
+    def getInternalIid(self, originalIid):
+        """Maps the given original ItemID to the corresponding internal ItemID.
+        """
+        return self.iidDict.getId(originalIid)
 
     def getMaxIid(self):
+        """Returns the highest internal assigned ItemID."""
         return self.iidDict.id - 1
 
     def getMaxUid(self):
+        """Returns the highest internal assigned UserID."""
         return self.uidDict.id - 1
