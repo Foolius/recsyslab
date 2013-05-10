6a8d3ab150db9988dc252c6b94c7608616af8a6f
Julius Kolbe <Foolius@server.fake>
2013-05-10 14:51:17 +0200

    Corrected the new imports.

--- bin/testing.py
+++ bin/testing.py
@@ -1,11 +1,11 @@
 def slopeone():
-    import reader
+    from util import reader
     r = reader.tabSepReader("u.data")
 
-    import split
+    from util import split
     train, test1 = split.split(r.getR(), 12320894329854567890)
 
-    import slopeone
+    from recommender import slopeone
     so = slopeone.slopeone(train)
 
     # import cPickle
@@ -17,20 +17,20 @@ def slopeone():
     # so = cPickle.load(inputfile)
     # inputfile.close()
 
-    import test
+    from util import test
     test.auc(test1, so.getRec, r)
 
 # slopeone()
 
 
 def svd():
-    import reader
+    from util import reader
     r = reader.tabSepReader("u.data")
 
-    import split
+    from util import split
     train, test1 = split.split(r.getR(), 1234567890)
 
-    import svd
+    from recommender import svd
     W, H = svd.learnModel(r.getMaxUid(), r.getMaxIid(),
                           0.0002, train, 1000, 25, r.numberOfTransactions)
     import numpy as np
@@ -41,21 +41,21 @@ def svd():
     # H = modelf['H']
     # modelf.close
 
-    import test
+    from util import test
     t = test.MFtest(W, H, train)
     test.hitrate(test1, t.getRec, 10)
 
-svd()
+# svd()
 
 
 def mf():
-    import reader
+    from util import reader
     r = reader.tabSepReader("u.data")
 
-    import split
+    from util import split
     train, test1 = split.split(r.getR(), 1234567890)
 
-    import mf
+    from recommender import mf
     W, H = mf.learnModel(r.getMaxUid(), r.getMaxIid(), 0.01, 0.01, 0.01,
                          0.1, train, 150, 3, r.numberOfTransactions,
                          mf.logLoss, mf.dLogLoss)
@@ -67,7 +67,7 @@ def mf():
     # H = modelf['H']
     # modelf.close
 
-    import test
+    from util import test
     t = test.MFtest(W, H, train)
     test.hitrate(test1, t.getRec, 10)
 
@@ -75,32 +75,32 @@ def mf():
 
 
 def knn():
-    import reader
+    from util import reader
     r = reader.tabSepReader("u.data")
 
-    import split
+    from util import split
     train, test1 = split.splitMatrix(r.getMatrix(), 1234567890)
 
-    import knn
+    from recommender import knn
     k = knn.itemKnn(train, 10)
 
-    import test
+    from util import test
     print test.auc(test1, k.getRec, r)
 
 # knn()
 
 
 def simple():
-    import reader
+    from util import reader
     r = reader.tabSepReader("u.data")
 
-    import split
+    from util import split
     train, test1 = split.split(r.getR(), 1234567890)
 
-    import primitive
+    from recommender import primitive
     c = primitive.randomRec(r.getR())
 
-    import test
+    from util import test
     test.auc(test1, c.getRec, r)
 
 # simple()
