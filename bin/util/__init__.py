f6ae57fd3230904c598c5be664a4c239a98f0005
Julius Kolbe <Foolius@server.fake>
2013-05-10 16:13:28 +0200

    Wrote the docstring for the util package.

--- bin/util/__init__.py
+++ bin/util/__init__.py
@@ -1,1 +1,8 @@
+"""Package with utility code to run the recommender algorithms.
+
+It contains the following modules:
+    reader  --  A reader to read in databases
+    split   --  Split databases
+    test    --  Test metrics
+    helper  --  Several helping methods and a two way dict"""
 pass
